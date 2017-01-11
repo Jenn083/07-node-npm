5ac4f06ef73e94f296c5ab0856dbcd3335c96a7f
jenn083 <nguyenj1203@gmail.com>
2017-01-10 20:57:19 -0800

    lab completed

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,27 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// Done: Initialize your project using NPM to create and populate a package.json file
+const express = require('express');
+// Done: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
-
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// Done: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
+  // Done: Using the response object, send the index.html file back to the user
 app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+  response.sendFile('new.html', {root: './public'});
 });
-
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// Done: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new.html', function(request, response){
+  response.sendFile('new.html', {root: './public'});
+});
+// app.get('/index.html',function(request, response){
+// //   response.status(404).sendFile('404',{root: './public'});
+// });
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // Done: Log to the console a message that lets you know which port your server has started on
+  console.log('Server is up and running on port 5000 and can be accessed at localhost: 3000 in your local browser');
 });
